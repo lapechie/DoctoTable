e4f4a8bf375df0c7dfa844bfc903bce884c07b77
Adam Grbac <adam.grbac@gmail.com>
2021-05-29 18:20:41 +1000

    Made DocxReader more OOPish; Made ConfigSpec a function; Removed Pandas

--- configspec.py
+++ configspec.py
@@ -1,12 +1,10 @@
-class configspec:
-
-    def read_spec(file):
-        """
-        Returns a list of columns for each spec
-        e.g [['Primary_Key', 'Feature_1', 'Feature_2', 'Feature_3'],
-             ['Primary_Key', 'Feature_5', 'Feature_6', 'Feature_7']]
-        """
-        specfile = open(file, "r")
+def read_spec(file):
+    """
+    Returns a list of columns for each spec
+    e.g [['Primary_Key', 'Feature_1', 'Feature_2', 'Feature_3'],
+         ['Primary_Key', 'Feature_5', 'Feature_6', 'Feature_7']]
+    """
+    with open(file, "r") as specfile:
         lines = specfile.read().splitlines()
         spec_list = [line.split(",") for line in lines]
-        return spec_list
+    return spec_list
